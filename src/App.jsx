c82477cf89108175f0a443d79c5507ef5e9e35ba
gkatai <katai.gergely@gmail.com>
2019-03-05 21:10:16 +0100

    feature(design): Main NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import Welcome from './pages/welcome/Welcome';
 import Login from './pages/authentication/Login';
 import Registration from './pages/authentication/Registration';
@@ -9,6 +11,20 @@ const App = () => {
   return (
     <BrowserRouter>
       <>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Mathpractice</Navbar.Brand>
+          <Nav className="ml-auto">
+            <NavLink to="/" className="nav-link">
+              Login as Guest
+            </NavLink>
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
+            <NavLink to="/registration" className="nav-link">
+              Register
+            </NavLink>
+          </Nav>
+        </Navbar>
         <Route path="/" exact component={Welcome} />
         <Route path="/login" component={Login} />
         <Route path="/registration" component={Registration} />
